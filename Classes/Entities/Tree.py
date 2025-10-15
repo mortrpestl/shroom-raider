5f1bab1d88a15200528f42f564db9382c14f03e0
mortrpestl <dlrmortera812@gmail.com>
2025-10-15 22:32:43 +0800

    feat: add Tree functionality

--- Classes/Entities/Tree.py
+++ Classes/Entities/Tree.py
@@ -1,4 +1,21 @@
 from Classes.Entity import Entity
 
+
 class Tree(Entity):
-    ...
+    def __init__(self, coord):
+        super().__init__(coord)
+        self.neighbors = set()
+
+    def find_neighbors(self,__grid_obj_map):
+        drcs = [(1,1),(1,-1),(-1,1),(-1,-1)]
+
+        def in_bounds(r,c): return 0<=r<len(__grid_obj_map) and 0<=c<len(__grid_obj_map[0])
+
+        for dr,dc in drcs:
+            r,c = self.get_pos()
+            nr,nc = r+dr,c+dc
+            if in_bounds(nr,nc):
+                neighbor = __grid_obj_map[nr][nc]
+                if isinstance(neighbor, Tree):
+                    self.neighbors.add(neighbor)
+                    
