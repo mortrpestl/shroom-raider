06546bfd6d851dd6c2480ce43646efeb66feae59
Francois Zingapan <b2025ftezingapan@pshs.edu.ph>
2025-10-18 14:22:19 +0800

    docs: add documentation for worked scripts.

--- Classes/Entities/Player.py
+++ Classes/Entities/Player.py
@@ -9,7 +9,20 @@ Flamethrower = items["Flamethrower"]
 Axe = items["Axe"]
 
 class Player(Entity):
-    def __init__(self, pos: list, on_grid: Grid, item: Entity | None = None): 
+    """Inherits Entity. Player acts as the user's representation in the game. 
+
+    Attributes:
+        See Base class.
+        item: an Entity (or None) that the Player is currently holding
+        mushroom_count: the number of mushrooms that the Player has collected
+    """
+    def __init__(self, pos: list, on_grid: Grid, item: Entity | None = None):
+        """Initializes a Player (Entity) with additional item param.
+
+        Args:
+            See Base class.
+            item: the item the Player is currently holding
+        """
         super().__init__(pos, on_grid)
         self.__item = item
         self.__mushroom_count = 0
@@ -17,18 +30,35 @@ class Player(Entity):
     # ! code below was generated using AI 
     # ! prompt: make me the getters and setters for the attributes of this class
     def get_item(self):
+        """Gets current item Player is holding
+        
+        Returns:
+            a boolean.
+        """
         return self.__item
 
     def set_item(self, item: Entity):
+        """Sets current item to a different item
+
+        Args:
+            item: an Entity
+        """
         self.__item = item
     
     def use_item(self):
+        """Uses item; thereby, setting it to None"""
         self.__item = None
 
     def get_mushroom_count(self):
+        """Gets current mushrooms Player has collected
+        
+        Returns:
+            a boolean.
+        """
         return self.__mushroom_count
 
     def increment_mushroom_count(self):
+        """Increases mushroom count by 1"""
         self.__mushroom_count += 1
 
 
--- Classes/Grid.py
+++ Classes/Grid.py
@@ -3,15 +3,32 @@ from Classes.Entities.import_entities import import_entities
 
 
 class Grid:
+    """Grid (or Level) arranges Entities and keeps track of Entities' positions.
+
+    Attributes:
+        GRID_LIST: A dictionary that stores Grids by name.
+        EMPTY_TILES: A set containing possible representations for empty tiles.
+        name: A str representing the name of Grid.
+        map_rows: An int that stores how many rows Grid has.
+        map_cols: An int that stores how many columns Grid has.
+        grid_vis_map: A list containing a representation of Grid usually from external text files.
+        grid_obj_map: A list containing the Entities arranged by their position values.
+        grid_user_display: A list containing the visual representation of Grid shown to the user.
+    """
     #you can add more keys (e.g. 'L🧑' to 'L🧑F' to allow 'F' to move Player)
     
-    GRID_LIST = dict() #stores levels by level name
-    #can add other looks for empty tiles for future use
-    EMPTY_TILES = {'.'}
+    GRID_LIST = dict()
+    EMPTY_TILES = {'.'} #can add other looks for empty tiles for future use
 
     def __init__(self, name, map_data: str):
         """
-        Converts map data to list of strings, initializes objects on map
+        Converts map data to an instance of Grid (initializes Entities on Grid).
+
+        Adds the new instance in GRID_LIST and connects adjacent trees for other functionality (namely, Flamethrower).
+
+        Args:
+            name: A str pertaining to the Grid's name.
+            map_data: A str listing the Grid's construction.
         """
 
         self.__name = name
@@ -36,8 +53,21 @@ class Grid:
 
     @classmethod
     def init_coord(cls, symbol, coord, entities, grid):
-        """
-        Given a symbol and coordinates, create instance of that item (if applicable)
+        """Given a symbol and coordinates, create an instance of that Entity (if applicable)
+
+        Args:
+            symbol: A string pertaining to the ASCII representation of a given Entity
+            coord: A list pertaining to the current coordinate
+            entities: A list that contains child classes of Entity
+            grid: A Grid instance that dictates where an Entity is
+        
+        Returns:
+            A tuple of length 2 containing
+            item_type: An Entity object (with appropriate type).
+            item_display_value: The visual representation of this Entity. 
+
+        Raises:
+            ValueError: if symbol does not represent any Entity
         """
         if symbol in Grid.EMPTY_TILES:
             return None, "　"
@@ -60,10 +90,14 @@ class Grid:
         
         item_type, item_display_value = item
 
-        
         return item_type(coord, grid), item_display_value
 
     def connect_trees(self, entities):
+        """Connects all adjacent trees in the Grid.
+
+        Args:
+            entities: A list that contains child classes of Entity.
+        """
         for r in range(self.__map_rows):
             for c in range(self.__map_cols):
                 cell = self.get_obj_in_coord(r,c)
@@ -72,16 +106,33 @@ class Grid:
                 
     @staticmethod
     def get_by_name(name):
-        """
-        Gets Grid instance by name.
+        """ Gets Grid instance by name.
+
+        Args:
+            name: a Grid's name
+
+        Returns:
+            a Grid object
+
+        Raises:
+            KeyError: if there is no valid name in GRID_LIST
         """
         if name not in Grid.GRID_LIST:
             raise KeyError(f'No grid found with name:{name}')
         return Grid.GRID_LIST[name]
 
-    def get_obj_in_coord(self,r,c):
-        """
-        Gets item type at certain coordinate
+    def get_obj_in_coord(self, r: int, c: int):
+        """Gets Entity at certain coordinates.
+
+        Args:
+            r: An int pertaining to the row coordinate
+            c: An int pertaining to the column coordinate
+
+        Returns:
+            An Entity (or None) object in the given coordinates
+
+        Raises:
+            IndexError: if coordinates ([r,c]) are out of bounds
         """
         from Classes.Entity import Entity
 
@@ -93,9 +144,7 @@ class Grid:
         return self.__grid_obj_map[r][c]
 
     def render(self):
-        """
-        Renders a given grid on the terminal
-        """
+        """Renders a given grid on the terminal"""
         
         #clears system file
         os.system('cls' if os.name=='nt' else 'clear')
